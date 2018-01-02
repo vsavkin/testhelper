eb60c4a08185bea115bdb3790b0b5b1ad33df011
vsavkin <avix1000@gmail.com>
2018-01-02 11:05:08 -0500

    implement google-friendly ngmodulefactoryloader

--- src/app/list.module.spec.ts
+++ src/app/list.module.spec.ts
@@ -1,10 +1,9 @@
-import {Compiler, Component, Injectable, NgModuleFactory, NgModuleFactoryLoader, Type} from '@angular/core';
+import {Compiler, Component, getModuleFactory, Injectable, NgModuleFactory, NgModuleFactoryLoader, Type} from '@angular/core';
 import {RouterTestingModule} from '@angular/router/testing';
-import {async, ComponentFixture, fakeAsync, getTestBed, TestBed, tick} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import {Router} from '@angular/router';
 import {SnackListComponent, SnackListModule, SnackListService} from './list.module';
 import {CompileMetadataResolver} from '@angular/compiler';
-import {MockResourceLoader} from '@angular/compiler/testing';
 
 export function bootstrapPage<T>(component: Type<T>, config: any, initialUrl: string = ''): T {
   @Component({
@@ -64,92 +63,71 @@ function advance(fixture: ComponentFixture<any>): void {
 
 
 fdescribe('ListModule', () => {
-  beforeEach(async(() => {
-    createSummaries().then(summaries => {
-      resetTestEnvironmentWithSummaries(summaries);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: NgModuleFactoryLoader,
+          useClass: GoogleFriendlySpyNgModuleFactoryLoader
+        }
+      ]
     });
-    // TestBed.configureTestingModule({
-    //   providers: [
-    //     { provide: NgModuleFactoryLoader, useClass: GoogleFriendlySpyNgModuleFactoryLoader}
-    //   ]
-    // });
-  }));
+  });
 
-
-  fit('should work (with lazy loading)', fakeAsync(() => {
+  it('should work (with lazy loading)', fakeAsync(() => {
     const list = bootstrapPage(SnackListComponent, {
       lazyLoaded: {
         'snacks': SnackListModule
       }
-    }, '/snacks/list')
+    }, '/snacks/list');
 
     expect(list.items).toEqual(['snack1', 'snack2']);
   }));
 
-  // it('should work (without lazy loading)', fakeAsync(() => {
-  //   const list = bootstrapPage(SnackListComponent, {
-  //     imports: [SnackListModule]
-  //   }, 'list');
-  //
-  //   expect(list.items).toEqual(['snack1', 'snack2']);
-  // }));
+  it('should work (without lazy loading)', fakeAsync(() => {
+    const list = bootstrapPage(SnackListComponent, {
+      imports: [SnackListModule]
+    }, 'list');
+
+    expect(list.items).toEqual(['snack1', 'snack2']);
+  }));
 });
 
-
-function resetTestEnvironmentWithSummaries(summaries?: () => any[]) {
-  const {platform, ngModule} = getTestBed();
-  TestBed.resetTestEnvironment();
-  // console.log('reset with summaries', summaries())
-  TestBed.initTestEnvironment(ngModule, platform, summaries);
-}
-
-function createSummaries() {
-  TestBed.configureTestingModule({imports: [SnackListModule]});
-  const summariesPromise = TestBed.compileComponents().then(() => {
-    const metadataResolver = TestBed.get(CompileMetadataResolver) as CompileMetadataResolver;
-    const summaries = [
-      metadataResolver.getNgModuleSummary(SnackListModule),
-      () => [
-        metadataResolver.getDirectiveSummary(SnackListComponent)
-      ]
-    ];
-    return () => summaries;
-  });
-
-  return summariesPromise;
-}
 
 @Injectable()
 export class GoogleFriendlySpyNgModuleFactoryLoader implements NgModuleFactoryLoader {
   private _stubbedModules: {[path: string]: Promise<NgModuleFactory<any>>} = {};
 
   set stubbedModules(modules: {[path: string]: any}) {
-    // const res: {[path: string]:  any} = {};
-    // for (const t of Object.keys(modules)) {
-    //   const m = modules[t];
-    //   const id = this.metadata.getNgModuleMetadata(m).id;
-    //   try {
-    //     res[t] = Promise.resolve().then(() => getModuleFactory(id));
-    //   } catch (e) {
-    //     res[t] = this.compiler.compileModuleAsync(modules[t]);
-    //   }
-    // }
-    // this._stubbedModules = res;
+    const res: {[path: string]: any} = {};
+    for (const t of Object.keys(modules)) {
+      const m = modules[t];
+      const id = this.metadata.getNgModuleMetadata(m).id;
+      /**
+       * Getting module factory by id. If doesn't work, compile it.
+       */
+      res[t] = Promise.resolve().then(() => getModuleFactory(id)).catch(() =>
+        this.compiler.compileModuleAsync(m)
+      );
+    }
+    this._stubbedModules = res;
   }
 
   get stubbedModules(): {[path: string]: any} { return this._stubbedModules; }
 
-  constructor(private compiler: Compiler) {
-    console.log('-----')
-    console.log((compiler as any)._delegate)
-  }
-
-  // TestingCompilerImpl clearCacheFor(moduleType)
   load(path: string): Promise<NgModuleFactory<any>> {
     if (this._stubbedModules[path]) {
       return this._stubbedModules[path];
     } else {
       return <any>Promise.reject(new Error(`Cannot find module ${path}`));
     }
+  }
+
+  private get compiler(): Compiler {
+    return TestBed.get(Compiler);
+  }
+
+  private get metadata(): CompileMetadataResolver {
+    return TestBed.get(CompileMetadataResolver);
   }
 }
